1dbdd81c5ca93293d6a8bf35ea3f6c48767f5ff2
Morgan Roff <roffmorgan@gmail.com>
2023-03-27 14:32:50 -0400

    Implement hello world

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::{net::{IpAddr, Ipv4Addr, SocketAddr}, error::Error};
 
-use axum::{response::IntoResponse, routing::get, Router};
+use axum::{
+    response::IntoResponse,
+    routing::{get},
+    Router, Server,
+};
 
 async fn hello_world() -> impl IntoResponse {
     String::from("Hello, world!")
 }
 
+fn create_router() -> Router {
+    Router::new().route("/hello", get(hello_world))
+}
+
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/hello", get(hello_world));
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = create_router();
 
     const PORT: u16 = 8080;
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), PORT);
-    println!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    Server::bind(&addr).serve(app.into_make_service()).await?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use axum_test::TestServer;
+
+    #[tokio::test]
+    async fn hello_works() {
+        let server = TestServer::new(create_router().into_make_service()).unwrap();
+
+        let response = server.get("/hello").await;
+
+        assert_eq!(response.text(), "Hello, world!");
+    }
 }
