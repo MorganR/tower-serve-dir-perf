d9e5ff656b877ec429b0dff1f4cd9d0ca9b7c2d9
Morgan Roff <roffmorgan@gmail.com>
2023-03-27 10:10:20 -0400

    Implement a hello-world server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+
+use axum::{Router, routing::get, response::IntoResponse};
+
+async fn hello_world() -> impl IntoResponse {
+    String::from("Hello, world!")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/hello", get(hello_world));
+
+    const PORT: u16 = 8080;
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), PORT);
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
