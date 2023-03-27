5d955b92f612348c96e4348634b2186af30cc1d6
Morgan Roff <roffmorgan@gmail.com>
2023-03-27 16:10:07 -0400

    Implement /serve_dir

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@ use std::{net::{IpAddr, Ipv4Addr, SocketAddr}, error::Error};
 
 use axum::{
     response::IntoResponse,
-    routing::{get},
+    routing::get,
     Router, Server,
 };
+use tower_http::services::ServeDir;
 
 async fn hello_world() -> impl IntoResponse {
     String::from("Hello, world!")
 }
 
 fn create_router() -> Router {
-    Router::new().route("/hello", get(hello_world))
+    Router::new()
+        .route("/hello", get(hello_world))
+        .nest_service("/serve_dir", ServeDir::new("static"))
 }
 
 #[tokio::main]
@@ -31,11 +34,33 @@ mod tests {
     use axum_test::TestServer;
 
     #[tokio::test]
-    async fn hello_works() {
+    async fn hello() {
         let server = TestServer::new(create_router().into_make_service()).unwrap();
 
         let response = server.get("/hello").await;
 
         assert_eq!(response.text(), "Hello, world!");
+    }
+
+    #[tokio::test]
+    async fn serve_dir_basic_html() -> Result<(), Box<dyn Error>> {
+        let server = TestServer::new(create_router().into_make_service()).unwrap();
+
+        let response = server.get("/serve_dir/basic.html").await;
+
+        let expected = tokio::fs::read("static/basic.html").await?;
+        assert_eq!(response.bytes(), expected);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn serve_dir_scout_webp() -> Result<(), Box<dyn Error>> {
+        let server = TestServer::new(create_router().into_make_service()).unwrap();
+
+        let response = server.get("/serve_dir/scout.webp").await;
+
+        let expected = tokio::fs::read("static/scout.webp").await?;
+        assert_eq!(response.bytes(), expected);
+        Ok(())
     }
 }
